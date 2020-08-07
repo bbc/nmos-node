67179deebc557f0bf6853315c44b421aca7d5384
Sam Mesterton-Gibbons <sam.mesterton-gibbons@bbc.co.uk>
2020-08-07 18:09:17 +0100

    Use agents from the apmm-agent group
    Replaces `apmm-slave` with the identical `apmm-agent` to remove
    problematic terminology.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import os
 
 # Basic metadata
 name = "nodefacade"
-version = "0.12.2"
+version = "0.12.2.post1"
 description = "BBC implementation of an AMWA NMOS Node API"
 url = "https://github.com/bbc/nmos-node"
 author = "Peter Brightwell"
